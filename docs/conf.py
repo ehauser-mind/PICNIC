d633879b2184a4fdf782ee6e9276db5eaff7661c
Mike Schmidt <mikeschmidt@schmidtgracen.com>
2024-10-21 20:46:26 -0400

    new autodoc infrastructure

---
+++ docs/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'PICNIC'
+copyright = '2024, The NYSPI MIND Brain Imaging Lab'
+author = 'The NYSPI MIND Brain Imaging Lab'
+release = '0.0.5'
+
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx_rtd_theme',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+
+# -- Include PICNIC source code in this documentation --------------------------
+import sys, os
+sys.path.insert(0, os.path.abspath('../src/picnic'))
