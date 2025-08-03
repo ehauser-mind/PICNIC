5eacd35dcacbc7b6c5589098f215ad0c6d671cee
Mike Schmidt <mikeschmidt@schmidtgracen.com>
2025-08-03 17:54:55 -0400

    Made build directory safe on github actions to allow a time stamp to be read

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -52,6 +52,7 @@ make -C docs html
 
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git config --global --add safe.directory /__w/PICNIC/PICNIC
 
 docroot=`mktemp -d`
 rsync -av "docs/_build/html/" "${docroot}/"
